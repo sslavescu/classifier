70655d8ec16a01b875c40ae2848f57e3456dc6b6
sslavescu <sorin@slavescu.info>
2023-11-13 13:18:12 +0000

    first commit

---
+++ src/main/java/reacsys/fmwk/service/ClassifierApplication.java
@@ -0,0 +1,13 @@
+package reacsys.fmwk.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/reacsys/fmwk/service/ClassifierApplicationTests.java
@@ -0,0 +1,13 @@
+package reacsys.fmwk.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
